3ce9d6dae57e80ca1b263d0f7510845566aa61de
Kamil Grela <kamil.grela@schibsted.pl>
2016-01-04 15:35:07 +0100

    Adding findById.

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var bodyParser = require('body-parser'),
     express = require('express'),
     app = express(),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    booksRepository = require('./books-repository');
 
 var collection = null;
-var url = 'mongodb://localhost:27017/bookinventory';
+var url = 'mongodb://localhost:27017/booksinventory';
 var collection = MongoClient.connect(url).then(function (db) {
-    return db.collection('stocks');
+    return db.collection('books');
 });
 
 var logRequest = function (req, res, next) {
@@ -19,7 +20,23 @@ app.use(logRequest);
 
 app.use(bodyParser.json());
 
-app.post('/stock', function (req, res, next) {
+app.get('/book/:isbn', function(req, res, next) {
+    collection
+        .then(function(result) {
+            return result.find({isbn: req.params.isbn}).toArray();
+        })
+        .then(function(result) {
+            if (result.length === 0) {
+                return next();
+            }
+            res.send(result[0]);
+        })
+        .catch(function (error) {
+            return next(error);
+        });
+});
+
+app.post('/book', function (req, res, next) {
     collection
         .then(function (result) {
             return result.updateOne({isbn: req.body.isbn}, {isbn: req.body.isbn, count: req.body.count}, {upsert: true});
