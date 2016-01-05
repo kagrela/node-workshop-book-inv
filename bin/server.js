1e11937aa6ab75843684da85a9741827885ad030
Kamil Grela <kamil.grela@schibsted.pl>
2016-01-05 09:38:55 +0100

    Bringing back server.

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 var booksRepository = require('../main/books-repository'),
     app = require('../main/app')(booksRepository);
 
-app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
